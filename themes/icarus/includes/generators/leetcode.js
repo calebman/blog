21cc9858074f598a2cdf8f08263be9a8fda1f884
JianhuiChen <chenjianhui0428@gmail.com>
2020-06-06 19:25:19 +0800

    更新 leetcode 板块布局，新增分组、推荐阅读模块

--- themes/icarus/includes/generators/leetcode.js
+++ themes/icarus/includes/generators/leetcode.js
@@ -10,30 +10,48 @@ const leetcodePath = path.join(process.cwd(), 'source/_leetcode');
 module.exports = function (hexo) {
     if (fs.existsSync(leetcodePath)) {
         const leetcodeObj = {}
-        hexo.extend.generator.register('leetcode', function (locals) {
+        hexo.extend.generator.register('leetcode-post', function (locals) {
             const questions = Object.values(leetcodeObj);
-            return pagination('leetcode/', questions, {
-                perPage: 1,
-                layout: ['leetcode', 'index'],
-                format: 'page/%d/',
+            return questions.map(o => Object.assign({}, {
+                path: o.path,
+                layout: ['leetcode_post'],
                 data: Object.assign({}, {
                     widgets: [
                         { type: 'profile', position: 'left' },
-                        { type: 'recent_posts', position: 'left' }
-                    ]
+                        { type: 'recent_leetcode', position: 'left', recent: loadRecentPosts(questions, o) }
+                    ],
+                    post: o
                 })
-            });
+            }))
+        });
+
+        const loadRecentPosts = (questions, cur) => {
+            const recentQuestions = questions.filter(o => o !== cur && o.group === cur.group)
+            return loadGroups(recentQuestions)[0]
+        }
+
+        hexo.extend.generator.register('leetcode-page', function (locals) {
+            const questions = Object.values(leetcodeObj)
+            const groups = loadGroups(questions)
+            return {
+                path: 'leetcode/index.html',
+                layout: ['leetcode'],
+                data: Object.assign({}, {
+                    groups: groups
+                })
+            }
         });
 
         // watch file and render it
         const handleFileChange = (name) => {
-            if(!/\.md$/.test(name)) {
+            if (!/\.md$/.test(name)) {
                 return
             }
             const path = `${leetcodePath}/${name}`
             const post = frontMatterParse(fs.readFileSync(path));
             hexo.render.render({ text: post._content, engine: 'md' }).then(result => {
                 leetcodeObj[path] = Object.assign({}, post, {
+                    path: `leetcode/posts/${name.substring(0, name.lastIndexOf('.md'))}/index.html`,
                     _content: result
                 });
             });
@@ -44,8 +62,23 @@ module.exports = function (hexo) {
         }).on('add', handleFileChange)
             .on('change', handleFileChange)
             .on('unlink', name => delete leetcodeObj[name]);
-        if(!process.argv[2].startsWith('s')) {
+        if (!process.argv[2].startsWith('s')) {
             hexo.on('exit', () => watcher.close())
         }
     }
+}
+
+const loadGroups = questions => {
+    const groups = {}
+    questions.forEach(o => {
+        const groupName = o.group || '其他'
+        groups[groupName] = groups[groupName] || []
+        groups[groupName].push(o)
+    })
+    return Object.keys(groups).map(k => {
+        return {
+            name: k,
+            posts: groups[k]
+        }
+    })
 }
